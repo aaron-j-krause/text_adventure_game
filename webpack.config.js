4301428a73dfb91f69e4727c16ce47dac4783f89
Aaron Krause <aaroncrows@Aarons-MacBook-Pro.local>
2017-01-12 19:07:00 -0800

    boiler

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const WebpackHtmlPlugin = require('webpack-html-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const paths = {
+  entry: `${__dirname}/src/index.js`,
+  build: `${__dirname}/build/`,
+  scripts: `${__dirname}/src/scripts/`,
+  style: `${__dirname}/src/stylesheets/`
+}
+
+module.exports = {
+  entry: paths.entry,
+  output: {
+    path: paths.build,
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: [paths.scripts, paths.entry],
+        loader: 'babel',
+        presets: ['react', 'es2015']
+      },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader","css-loader") }
+    ]
+  },
+
+  plugins: [new WebpackHtmlPlugin({filename: 'index.html', inject: true}),
+            new ExtractTextPlugin('style.css')]
+}
